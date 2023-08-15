284ab15ebddfc57bdf6ee2162f286ea595dee90f
AlexanderSvidin <learnstudy334@gmail.com>
2023-08-15 21:53:40 +0300

    2.5 Практическая работа
    Задание 1. Генерация списка

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+number = int(input("Введите число: "))
+odd_nums_list = []
+
+for num in range(1, number + 1):
+    if num % 2 != 0:
+        odd_nums_list.append(num)
+
+print("Список из нечётных чисел от одного до N:", odd_nums_list)
