f016a3c770be31da18b62f7c7d5fcce736b5d009
AlexanderSvidin <learnstudy334@gmail.com>
2023-08-24 14:00:24 +0300

    2.5 Практическая работа
    Задание 8. Сортировка

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-text = input("Введите слово: ")
-reverse_text = ""
-for sym in text:
-    reverse_text = sym + reverse_text
+# Алгоритм быстрой сортировки Хоара
 
-if text == reverse_text:
-    print("Слово является палиндромом")
-else:
-    print("Слово не является палиндромом")
+import random
+
+
+def QuickSort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = random.choice(array)
+        less = [elem for elem in array if elem < pivot]
+        equal = [elem for elem in array if elem == pivot]
+        greater = [elem for elem in array if elem > pivot]
+
+        return QuickSort(less) + equal + QuickSort(greater)
+
+
+nums_list = [1, 4, -3, 0, 10]
+print("Изначальный список:", nums_list)
+print("Отсортированный список:", QuickSort(nums_list))
