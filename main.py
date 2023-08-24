dccd72db8304aede1008c685b72e826b13665947
AlexanderSvidin <learnstudy334@gmail.com>
2023-08-24 14:34:00 +0300

    2.5 Практическая работа
    Задание 9. Обратный анализ чётных чисел

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# Алгоритм быстрой сортировки Хоара
+nums_list = []
+count_elem = int(input('Введите кол-во элементов списка: '))
 
-import random
+for index_elem in range(count_elem):
+    print("Введите", str(index_elem + 1) + "-й элемент списка:", end=" ")
+    elem = int(input())
+    nums_list.append(elem)
 
+def even_reverse_nums(array):
+    for index_elem in range(len(array) - 1, -1, -1):
+        if array[index_elem] % 2 == 0:
+            print(array[index_elem], end=" ")
 
-def QuickSort(array):
-    if len(array) <= 1:
-        return array
-    else:
-        pivot = random.choice(array)
-        less = [elem for elem in array if elem < pivot]
-        equal = [elem for elem in array if elem == pivot]
-        greater = [elem for elem in array if elem > pivot]
-
-        return QuickSort(less) + equal + QuickSort(greater)
-
-
-nums_list = [1, 4, -3, 0, 10]
-print("Изначальный список:", nums_list)
-print("Отсортированный список:", QuickSort(nums_list))
+print("\nИзначальный список:", nums_list)
+print("Чётные числа в обратном порядке:", end=" ")
+even_reverse_nums(nums_list)
