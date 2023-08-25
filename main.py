605b9344ed2d6dd581cb15f2f8437e2721befecb
AlexanderSvidin <learnstudy334@gmail.com>
2023-08-25 20:48:02 +0300

    3.5 Практическая работа
    Задание 1. Страшный код

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-nums_list = []
-count_elem = int(input('Введите кол-во элементов списка: '))
+a = [1, 5, 3]
+b = [1, 5, 1, 5]
+c = [1, 3, 1, 5, 3, 3]
 
-for index_elem in range(count_elem):
-    print("Введите", str(index_elem + 1) + "-й элемент списка:", end=" ")
-    elem = int(input())
-    nums_list.append(elem)
+a.extend(b)
+print('Количество цифр 5 при первом объединении:', a.count(5))
 
-def even_reverse_nums(array):
-    for index_elem in range(len(array) - 1, -1, -1):
-        if array[index_elem] % 2 == 0:
-            print(array[index_elem], end=" ")
+for elem in a:
+    if elem == 5:
+        a.remove(5)
 
-print("\nИзначальный список:", nums_list)
-print("Чётные числа в обратном порядке:", end=" ")
-even_reverse_nums(nums_list)
+a.extend(c)
+print('Количество цифр 3 при втором объединении:', a.count(3))
+print('Итоговый список:', a)
